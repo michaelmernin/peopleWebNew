42be1cd72915d9c936380ac3aabf66a298160d2a
michaelmernin <michael.mernin@gmail.com>
2017-02-12 18:23:33 -0600

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,37 @@
 package com.company;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import com.sun.tools.internal.ws.processor.model.Model;
 import spark.ModelAndView;
-import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-
-
-
-
-
+    public static ArrayList<Person> person = new ArrayList<>();
 
 
     public static void main(String[] args) throws IOException {
         ArrayList<Person> people = new ArrayList<>();
         File data = new File("people.csv");
         Scanner scanner = new Scanner(data);
-        while(scanner.hasNext()) {
+        while (scanner.hasNext()) {
             String read = scanner.nextLine();
             //read.split(","); unnecessary, redundant
-            String [] persons = read.split(",");
+            String[] persons = read.split(",");
+            if(!"id".equals(persons[0]));
             Person personList = new Person(Integer.parseInt(persons[0]), persons[1], persons[2], persons[3], persons[4], persons[5]);
             people.add(personList);
         }
+
 
         //public static void personList() {
         //people.add(new Person(personList());
@@ -41,10 +43,14 @@ public class Main {
         // a = the list, a is the class(object), y variable given to the entry that is pulled out
 
 
-
         Spark.get(
                 "/",
                 ((request, response) -> {
+
+                    String offsetString = request.queryParams("offset");
+                    if(offsetString == null) {
+                        offsetString = "0";
+                    }
                     //to disregard first post on the txt file
                     //if(!"id".equals(lineParts[0])) {
 
@@ -58,45 +64,55 @@ public class Main {
                     //put temp list in the hashmap
                     //put next in hashmap(offset + 20)
                     //put previous in hashmap(offset -20)
+                    String offSet = request.queryParams("offSet");
+
+                    int offer = Integer.parseInt(offSet);
+                    List<Person> personOff = new ArrayList<>();
+                    personOff = person.subList(offSet, offSet + 20);
+
+                    HashMap map = new HashMap<>();
+
+                    map.put("People", personOff);
+                    map.put("nextOffset", offSet +20);
+                    map.put("prevOffset", offSet -20);
+
+
+
 
                     String personList = people.get("firstName", "lastName", personList);
-                   // File data = new File("people.csv")
+                    // File data = new File("people.csv")
                     // String personList = Person();
-                    while(people.hasNext()) {
+                 //   while (people.hasNext()) {
 
-                        people.add(personList);
+                      //  if (!id.equals(lineParts[0])) {
 
-                        response.redirect("/");
+                          //  people.add(personList);
 
-                        return "";
-                })
-                return new MustacheTemplateEngine()
-        );
+                          //  response.redirect("/");
 
+                            return new ModelAndView(personList, "main.html");
+                        }
+                        return new MustacheTemplateEngine()
 
-        Spark.get(
-                "/person",
-                ((request, response) -> {
-                    String peopleList = ;
-                    ArrayList<Person> listed = new ArrayList<>();
-                    for(Person id : people)
-                        if()
-
-
+                        Spark.get(
+                                "/person",
+                                ((request, response) -> {
+                                    String peopleList =  ;
+                                    ArrayList<Person> listed = new ArrayList<>();
+                                    for (Person id : people)
+                                        if ()
 
 
+                                            return new ModelAndView(listed, "person.html");
+                                }),
+
+                                new MustacheTemplateEngine()
+                        );
 
 
-                   return new ModelAndView(listed, "person.html");
-                })
-        )
+                    }
 
 
-
-
-
-    }
-
-
+                }));
     }
 }
